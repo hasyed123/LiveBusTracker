4745651d213d8b9c3c8f39290027b7c39e602248
Hasan Syed <hsyed@algomau.ca>
2023-01-12 16:12:36 -0500

    Simple UI

--- app/src/main/java/com/example/bramptonbuslivetracker/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/bramptonbuslivetracker/ui/main/MainActivity.kt
@@ -5,13 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bramptonbuslivetracker.ui.detail.DetailActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -40,21 +50,37 @@ class MainActivity : AppCompatActivity() {
         val routeList = viewModel.routeList.observeAsState()
         routeList.value?.let {
             LazyColumn {
-                items(it) { routeNumber ->
-                    tripCard(routeNumber)
+                items(it.map{ it.toInt() }.sorted()) { routeNumber ->
+                    routeCard(routeNumber.toString())
                 }
             }
         }
     }
 
     @Composable
-    fun tripCard(routeNumber: String) {
-        Text(
-            text = routeNumber,
-            modifier = Modifier.clickable {
-                startDetailActivity(routeNumber)
-            }
-        )
+    fun routeCard(routeNumber: String) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(color = Color.LightGray)
+                .clickable {
+                    startDetailActivity(routeNumber)
+                }
+        ) {
+            Text(
+                text = routeNumber,
+                fontSize = 30.sp,
+                color = Color.Blue
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun previewRouteCard() {
+        routeCard("34")
     }
 }
 
